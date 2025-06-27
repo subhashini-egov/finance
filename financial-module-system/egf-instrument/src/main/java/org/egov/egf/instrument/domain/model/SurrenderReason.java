c0a296b5321b6f44cfeada3fddd4641a153e36a0
Subhashini Srinivasan <subhashini.srinivasan@egovernments.org>
2025-06-27 15:57:54 +0530

    Added finance system from Urban

---
+++ financial-module-system/egf-instrument/src/main/java/org/egov/egf/instrument/domain/model/SurrenderReason.java
@@ -0,0 +1,38 @@
+package org.egov.egf.instrument.domain.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
+import org.egov.common.domain.model.Auditable;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class SurrenderReason extends Auditable {
+    /**
+     * id is the unique Identifier of the reason
+     */
+    private String id;
+    /**
+     * name is the reason of instrument surrender. Example "Damaged cheque", "Cheque to be scrapped" etc
+     */
+    @NotBlank
+    @Size(max = 50, min = 5)
+    private String name;
+    /**
+     * description is detailed description of the surrender of a instrument
+     */
+    @NotBlank
+    @Size(max = 250)
+    private String description;
+
+}
